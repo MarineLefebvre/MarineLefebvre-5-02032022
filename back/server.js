a95a67cdc10e411ea05f40ac145da37bdf9d222f
Marine <tech-03@kerlan-info.fr>
2022-03-08 09:58:52 +0100

    liste des canapés sur la page d'accueil

--- back/server.js
+++ back/server.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const app = require('./app');
+var cors = require('cors');
+app.use(cors({origin : "*"}));
 
 const normalizePort = val => {
   const port = parseInt(val, 10);
---
+++ front/js/script.js
@@ -0,0 +1,45 @@
+let blockOfCanape= document.getElementById("items");
+let listCanape;
+
+fetch("http://localhost:3000/api/products")
+    .then(function(res) {
+        if (res.ok) {
+            return res.json();
+        }
+    })
+    .then(function(value) {
+        console.log(value);
+        listCanape = value;
+        generateCardCanape();
+    })
+    .catch(function(err) {
+        //alert("Une erreur est survenue ....")
+        alert(err)
+    });
+
+function generateCardCanape() {
+    console.log(blockOfCanape);
+    for (let canape of listCanape){
+        let canapeCard = document.createElement("a");
+        canapeCard.href = "./product.html?id="+canape._id;
+        let canapeArticle = document.createElement("article");
+        let text = document.createTextNode(canape.name);
+        let img = document.createElement("img");
+        img.src = canape.imageUrl;
+        img.alt = canape.altTxt;
+        let canapeTitre = document.createElement("h3");
+        canapeTitre.className = "productName";
+        canapeTitre.appendChild(text);
+        let canapeDescription = document.createElement("p");
+        canapeDescription.className = "productDescription";
+        let description = document.createTextNode(canape.description);
+        canapeDescription.appendChild(description);
+        canapeArticle.appendChild(img);
+        canapeArticle.appendChild(canapeTitre);
+        canapeArticle.appendChild(canapeDescription);
+        canapeCard.appendChild(canapeArticle);
+        blockOfCanape.appendChild(canapeCard);
+
+    }
+}
+
